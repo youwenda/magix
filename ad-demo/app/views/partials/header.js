5769074c8d65c92a9b3507b92a39b544db23cbea
霍雍 <huoyong.msb@alibaba-inc.com>
2013-10-22 14:08:20 +0800

    moved ad-demo to another repo

--- ad-demo/app/views/partials/header.js
+++
@@ -1,9 +0,0 @@
-KISSY.add("app/views/partials/header", function(S, View) {
-    return View.extend({
-        render: function() {
-            this.setViewHTML(this.template);
-        }
-    });
-}, {
-    requires: ['magix/view']
-});
