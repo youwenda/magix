d4af23601c448a7bb186ec759546062b805e45b4
xinglie.lkf <xinglie.lkf@taobao.com>
2014-03-20 17:06:59 +0800

    bug fix

--- dist/1.1/kissy-magix.js
+++ dist/1.1/kissy-magix.js
@@ -1687,7 +1687,7 @@ Mix(Mix(Vframe.prototype, Event), {
             var path = Magix.pathToObject(viewPath);
             var vn = path.pathname;
             var sign = --me.sign;
-            Magix.libRequire(vn, function(View) {
+            Magix.use(vn, function(View) {
                 if (sign == me.sign) { //有可能在view载入后，vframe已经卸载了
 
                     BaseView.prepare(View);
